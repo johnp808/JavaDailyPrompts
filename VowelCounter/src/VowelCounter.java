a86e9e9d3045a093f5cbddafa12a8d532db08bad
johnp808 <johnpuliatch@gmail.com>
2025-05-28 21:25:03 -1000

    included counters for each vowel

--- VowelCounter/src/VowelCounter.java
+++ VowelCounter/src/VowelCounter.java
@@ -4,7 +4,8 @@ public class VowelCounter {
 
 	private Scanner sc = new Scanner(System.in);
 	private String userChoice, userSentence = "";
-	private int vowelCounter = 0;
+	private int vowelCounter, a, e, i, o, u = 0;
+	private boolean running = true;
 	
 	public static void main(String[] args) {
 		VowelCounter vC = new VowelCounter();
@@ -15,37 +16,42 @@ public class VowelCounter {
 		// take in user input for a sentence
 		System.out.println("Please Enter A Sentence\nI Will Then Count The Vowels In It!");
 		//ignore case
-		userSentence = "Hello buddy"; // 3 vowels in example
-//		userSentence = sc.nextLine();
-		char[] userSentenceArray = userSentence.toLowerCase().toCharArray();
-		// count the amount of vowels in the sentence
-		for(int i = 0; i < userSentenceArray.length -1; i++) {
-			char letter = userSentenceArray[i];
-			
-			if(letter == 'a') {
-				vowelCounter++;
+//		do {
+			userSentence = "Hello buddy"; // 3 vowels in example
+	//		userSentence = sc.nextLine();
+			char[] userSentenceArray = userSentence.toLowerCase().toCharArray();
+			// count the amount of vowels in the sentence
+			for(int i = 0; i < userSentenceArray.length -1; i++) {
+				char letter = userSentenceArray[i];
+				
+				if(letter == 'a') {
+					vowelCounter++;
+					a++;
+				}
+				else if(letter == 'e') {
+					vowelCounter++;
+					e++;
+				}
+				else if(letter == 'i') {
+					vowelCounter++;
+					i++;
+				}
+				else if(letter == 'o') {
+					vowelCounter++;
+					o++;
+				}
+				else if(letter == 'u') {
+					vowelCounter++;
+					u++;
+				}
+	//			System.out.print(letter + ", ");
+	//			System.out.println();
 			}
-			else if(letter == 'e') {
-				vowelCounter++;
-			}
-			else if(letter == 'i') {
-				vowelCounter++;
-			}
-			else if(letter == 'o') {
-				vowelCounter++;
-			}
-			else if(letter == 'u') {
-				vowelCounter++;
-			}
-			else {
-				continue;
-			}
-			System.out.print(letter + ", ");
-			System.out.println();
-		}
-		//display the total vowel count at the end		
-		System.out.println(vowelCounter);
-		System.out.println(userSentence);
-		
+			//display the total vowel count at the end		
+			System.out.println("There Are " + vowelCounter + " Vowels In " + userSentence + "!");
+			System.out.println("There Are " + a + " 'A's, " + e + " 'E's, " + i + " 'I's, " 
+					 			+ o + " 'O's, " + u + " 'U's, " + "In " + userSentence + "!");
+//			running = false;
+//		} while(true);
 	}
 }
