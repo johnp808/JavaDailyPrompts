9f58cb84921febe450fbfd702558fd393730150e
johnp808 <johnpuliatch@gmail.com>
2025-06-05 18:02:54 -1000

    Created prompt for 6-5-25 'Longest Common Prefix' stubbed out the run method

---
+++ 15-LongestCommonPrefix/src/LongestCommonPrefix.java
@@ -0,0 +1,17 @@
+
+public class LongestCommonPrefix {
+
+	private String [] input = new String[10];
+	public static void main(String[] args) {
+		LongestCommonPrefix lCP = new LongestCommonPrefix();
+		lCP.run();
+	}
+	
+	public void run() {
+		input[1] = "flower";
+		input[2] = "flow";
+		input[3] = "flight";
+	}
+	
+	
+}
