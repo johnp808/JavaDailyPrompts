a89c27671f0da3c778733690d655af84348b31c0
johnp808 <johnpuliatch@gmail.com>
2025-06-05 19:07:07 -1000

    created the logic for the app, next is making the user input for the program

--- 15-LongestCommonPrefix/src/LongestCommonPrefix.java
+++ 15-LongestCommonPrefix/src/LongestCommonPrefix.java
@@ -2,16 +2,44 @@
 public class LongestCommonPrefix {
 
 	private String [] input = new String[10];
+	private String firstWord = "", prefix = "";
+	private char letter = ' ';
 	public static void main(String[] args) {
 		LongestCommonPrefix lCP = new LongestCommonPrefix();
 		lCP.run();
 	}
 	
 	public void run() {
-		input[1] = "flower";
-		input[2] = "flow";
-		input[3] = "flight";
+		enterWords();
+		loopWords();
 	}
 	
+	public void enterWords() {
+		
+	}
 	
+	public void loopWords() {
+		
+		if(input.length == 0 || input[0] == null) {
+			System.out.println("No Common Prefix.");
+			return;
+		}
+		
+		firstWord = input[0];
+		
+		for (int i = 0; i < firstWord.length(); i++) {
+			letter = firstWord.charAt(i); // f for fire
+				for (int j = 1; j < input.length; j++) {
+					// f for fighter
+					if (input[j] != null) {
+						if (i >= input[j].length() || input[j].charAt(i) != letter) {
+							System.out.println("Longest Common Prefix: " + prefix);
+							return;
+						}
+					}
+				}
+				prefix += letter;
+		}
+		System.out.println(prefix);
+	}
 }
