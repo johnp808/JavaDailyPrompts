975f64a6644c835aa8d5e17887e949d9bf43450b
johnp808 <johnpuliatch@gmail.com>
2025-06-09 00:22:25 -1000

    Completed Fibonacci number generator prompt and added README.md

---
+++ 17-FibonacciNumGenerator/README.md
@@ -0,0 +1,21 @@
+# Fibonacci Number Generator
+
+### 6-8-25
+
+## Goal: Practice working with loops, conditionals, and input validation. Reinforce understanding of number sequences and how to maintain application state across multiple user runs.
+
+# Prompt
+
+## Create a program that takes a positive integer input n and generates the first n numbers in the Fibonacci sequence. Allow the user to input multiple times, and handle invalid inputs gracefully.
+
+### Prompts user for a positive integer.
+
+### Displays the corresponding number of Fibonacci numbers.
+
+### Handles edge cases (n = 1, n = 2, n > 2) correctly.
+
+### Repeats until the user chooses to exit.
+
+### Input validation for non-integer or invalid entries.
+
+### Resets values between runs to avoid carryover.
--- 17-FibonacciNumGenerator/src/FibNumGen.java
+++ 17-FibonacciNumGenerator/src/FibNumGen.java
@@ -1,12 +1,85 @@
+import java.util.Scanner;
 
 public class FibNumGen {
 
+	private int first = 0, second = 1, next = 0, input = 0;
+	private Scanner sc = new Scanner(System.in);
+	private String output = "", userInput;
+	
 	public static void main(String[] args) {
 		FibNumGen fNG = new FibNumGen();
 		fNG.run();
 	}
 	
 	public void run() {
+		do {
+			reset();
+			System.out.println("Please Enter How Many Fibonacci Numbers You Would Like To Generate:");
+			userInput();
+			calculate();
+			System.out.println("Input: " + input);
+			System.out.println("Result: " + output + "\n");
+		} while(playAgain());
+	}
+	
+	public void userInput() {
+		while(true) {
+			try {
+				input = Integer.parseInt(sc.nextLine());
+				if(input <= 0) {
+					System.out.println("Please Enter A Number Greater Than 0...");
+				}
+				else {
+					break;
+				}
+			}
+			catch(Exception exc) {
+				System.out.println("Please Enter A Valid Number...");
+			}
+		}
+	}
+	
+	public void calculate() {
 		
+		if(input == 1) {
+			output = "" + first;
+		}
+		else if(input == 2) {
+			output = first + " " + second;
+		}
+		else if (input > 2) {
+			output = first + " " + second;
+			for(int i = 0; i < input - 2; i++) {
+			next = first + second;
+			output += " " + next;
+			first = second;
+			second = next;
+			}
+		}
+	}
+	
+	public boolean playAgain() {
+		while(true) {
+			System.out.println("Would You Like To Enter Another Number?\n1) Yes\n2) No");
+			userInput = sc.nextLine();
+			if(userInput.equals("1")) {
+				return true;
+			}
+			else if(userInput.equals("2")) {
+				System.out.println("GoodBye");
+				return false;
+			}
+			else {
+				System.out.println("\nPlease Enter A Valid Input\n");
+			}
+		}
+	}
+	
+	public void reset() {
+		first = 0;
+		second = 1;
+		next = 0;
+		input = 0;
+		output = "";
 	}
 }
