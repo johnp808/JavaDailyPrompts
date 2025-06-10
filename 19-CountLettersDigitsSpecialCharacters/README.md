c99be61e260b30c880b214c1148f642947264731
johnp808 <johnpuliatch@gmail.com>
2025-06-09 22:57:52 -1000

    Completed prompt for 6-9-25 added README.md

---
+++ 19-CountLettersDigitsSpecialCharacters/README.md
@@ -0,0 +1,21 @@
+# Count Letters, Digits, and Special Characters
+
+### 6-9-25
+
+## Goal: Practice string parsing and character classification using loops and conditionals. Strengthen familiarity with Character class methods and custom counters.
+
+# Prompt
+
+## Write a program that prompts the user to enter a sentence. The program should count and display how many letters, digits, and special characters are in the sentence (excluding whitespace). After displaying the results, the user should be asked if they want to enter another sentence.
+
+### Prompts the user to enter a sentence or phrase.
+### Iterates through each character of the string.
+### Uses Character.isLetter() and Character.isDigit() to identify character types.
+### Any non-letter, non-digit, non-space character is counted as a special character.
+
+### Displays the counts for:
+### Total letters
+### Total digits
+### Total special characters
+
+### Repeats until the user selects to exit.
--- 19-CountLettersDigitsSpecialCharacters/src/CountLetterDigitSpecialChar.java
+++ 19-CountLettersDigitsSpecialCharacters/src/CountLetterDigitSpecialChar.java
@@ -4,7 +4,7 @@ public class CountLetterDigitSpecialChar {
 
 	private Scanner sc = new Scanner(System.in);
 	private int charCount = 0, digitCount = 0, sCharCount = 0;
-	private String sentence = "";
+	private String sentence = "", choice = "";
 	
 	public static void main(String[] args) {
 		CountLetterDigitSpecialChar sR = new CountLetterDigitSpecialChar();
@@ -12,24 +12,62 @@ public class CountLetterDigitSpecialChar {
 	}
 	
 	public void run() {
-		System.out.println("Please Enter A Sentence");
-		sentence = "Hello World! 123 :)";
-		
+		do {
+			reset();
+			userInput();
+			calculate();
+		} while(playAgain());
+	}
+	
+	public void userInput() {
+		System.out.println("Please Enter A Sentence...");
+		sentence = sc.nextLine();
+	}
+	
+	public void calculate() {
 		for(int i = 0; i < sentence.length(); i++) {
 			char c = sentence.charAt(i);
 			
 			if(Character.isLetter(c)) {
-				System.out.println("letter: " + c);
 				charCount++;
 			}
 			else if(Character.isDigit(c)) {
-				System.out.println("Digit: " + c);
 				digitCount++;
 			}
 			else if(c != ' ') {
-				System.out.println("s Char: " + c);
 				sCharCount++;
 			}
 		}
+		if(charCount == 0 && digitCount == 0 && sCharCount == 0) {
+			System.out.println("You Didn't Enter Anything...\n");
+		}
+		else {
+			System.out.println("Letters: " + charCount + "\nDigits: " + digitCount + "\nSpecial Characters: " + sCharCount + "\n");
+		}
+	}
+	
+	public boolean playAgain() {
+		while(true) {
+			System.out.println("Would You Like To Play Again?\n1) Yes\n2) No");
+			choice = sc.nextLine();
+			if(choice.equals("1")) {
+				return true;
+			}
+			else if(choice.equals("2")) {
+				System.out.println("Goodbye!");
+				return false;
+			}
+			else {
+				System.out.println("Please Choose One Of The Options...\n");
+			}
+		}
+	}
+	
+	public void reset() {
+		charCount = 0; 
+		digitCount = 0; 
+		sCharCount = 0;
+		sentence = "";
+		choice = "";
 	}
 }
