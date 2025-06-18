cfc94e47660304325c83d8d89a9bcbae72a0ca1f
johnp808 <johnpuliatch@gmail.com>
2025-06-18 05:57:30 -1000

    Created prompt for 6-17-25 'Unique Character Checker' and fleshed out methods

---
+++ 25-UniqueCharacterChecker/src/com/john/UniqueLetterChecker.java
@@ -0,0 +1,29 @@
+package com.john;
+
+public class UniqueLetterChecker {
+
+	public static void main(String[] args) {
+		UniqueLetterChecker uLC = new UniqueLetterChecker();
+		uLC.run();
+	}
+	
+	public void run() {
+		do {
+			userInput();
+			check();
+		} while(runAgain());
+		
+	}
+	
+	public void userInput() {
+		
+	}
+	
+	public void check() {
+		
+	}
+	
+	public boolean runAgain() {
+		return true;
+	}
+}
