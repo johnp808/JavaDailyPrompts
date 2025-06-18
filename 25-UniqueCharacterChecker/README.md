3cf11e823201ca10a2d5c4f84dd8ac778d8f3360
johnp808 <johnpuliatch@gmail.com>
2025-06-18 06:35:18 -1000

    finished 6-17-25 prompt and added a README.md

---
+++ 25-UniqueCharacterChecker/README.md
@@ -0,0 +1,13 @@
+# Unique Character Checker
+
+### 6-17-25
+
+## Goal: Practice working with Set, loops, and string manipulation. Reinforce understanding of how to identify and track unique characters in a string.
+
+# Prompt Write a program that checks whether a given word contains all unique characters (i.e., no repeating characters). The user will enter a single word, and your program will return whether the word contains all unique letters.
+
+### Prompt the user to enter a single word (no spaces).
+### If the input is invalid (contains numbers, symbols, or spaces), ask again.
+### Check if all characters in the word are unique.
+### Print the result: "All characters are unique." or "Duplicate characters found."
+### Ask if the user wants to check another word.
--- 25-UniqueCharacterChecker/src/com/john/UniqueLetterChecker.java
+++ 25-UniqueCharacterChecker/src/com/john/UniqueLetterChecker.java
@@ -1,7 +1,15 @@
 package com.john;
 
-public class UniqueLetterChecker {
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class UniqueLetterChecker {
+	private Scanner sc = new Scanner(System.in);
+	private String word = "", userInput = "";
+	private Set<Character> uniqueChars = new LinkedHashSet<Character>();
+	private boolean dupFound = true;
+	
 	public static void main(String[] args) {
 		UniqueLetterChecker uLC = new UniqueLetterChecker();
 		uLC.run();
@@ -11,19 +19,73 @@ public class UniqueLetterChecker {
 		do {
 			userInput();
 			check();
-		} while(runAgain());
+			result();
+		}while(runAgain());
 		
 	}
 	
 	public void userInput() {
-		
+		System.out.print("Please Enter A Word: ");
+		while(true) {
+			word = sc.nextLine().trim();
+			if (word.isBlank() || word.matches(".*[^a-zA-Z0-9 ].*") || word.matches(".*\\d.*")) {
+				System.out.println("\nPlease Use Letters Only...");
+				continue;
+			}
+			else {
+				break;
+			}
+		}
 	}
 	
 	public void check() {
-		
+		dupFound = false;
+		for (int i = 0; i < word.length(); i++) {
+			char letter = word.charAt(i);
+			if (uniqueChars.contains(letter)) {
+				dupFound = true;
+				break;
+			}
+			else {
+				uniqueChars.add(letter);
+			}
+		}
+	}
+	
+	public void result() {
+		System.out.println();
+		if (dupFound) {
+			System.out.println("Duplicate Characters Were Found.");
+		}
+		else {
+			System.out.println("All characters are unique");
+		}
 	}
 	
 	public boolean runAgain() {
-		return true;
+		System.out.println("\nWould You Like To Enter Another Word?\n1) Yes\n2) No");
+		while (true) {
+			userInput = sc.nextLine();
+			if (userInput.equals("1")) {
+				System.out.println();
+				reset();
+				return true;
+			}
+			else if (userInput.equals("2")) {
+				System.out.println("Goodbye");
+				return false;
+			}
+			else {
+				System.out.println("Please Select A Valid Option...");
+			}
+		}
 	}
+	
+	public void reset() {
+		word = "";
+		userInput = "";
+		dupFound = true;
+		uniqueChars.clear();
+	}
+	
 }
