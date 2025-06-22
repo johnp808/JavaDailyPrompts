4263fbdd2a3222073853a35c415727ac891b95ca
johnp808 <johnpuliatch@gmail.com>
2025-06-22 12:58:21 -1000

    Created boilerplate for prompt 6-22-25 'Consonant To Vowel Ratio'

---
+++ 30-ConsonantToVowelRatio/src/com/john/RatioGen.java
@@ -0,0 +1,44 @@
+package com.john;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RatioGen {
+
+	private Scanner sc = new Scanner(System.in);
+	private String sentence = "", userInput = "", word = "";
+	private ArrayList<Character> cons = new ArrayList<Character>(), vowels = new ArrayList<Character>();
+	private double ratio = 0;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void userInput() {
+		
+	}
+	
+	public void count() {
+		
+	}
+	
+	public void ratio() {
+		
+	}
+	
+	public void printResults() {
+		
+	}
+	
+	public boolean runAgain() {
+		return true;
+	}
+	
+	public void reset() {
+		
+	}
+}
