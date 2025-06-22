591725c3300fe03622e2b806ae6afd5dac18f20c
johnp808 <johnpuliatch@gmail.com>
2025-06-21 22:40:49 -1000

    prompt for 6-21-25 'Word Length Classifier' setup boilerplate code

---
+++ 28-WordLengthClassifier/src/com/john/WordLengthClassifier.java
@@ -0,0 +1,29 @@
+package com.john;
+
+public class WordLengthClassifier {
+
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void userInput() {
+		
+	}
+	
+	public void sortWords() {
+		
+	}
+	
+	public boolean runAgain() {
+		return true;
+	}
+	
+	public void reset() {
+		
+	}
+}
