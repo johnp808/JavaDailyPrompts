f5a912184bf8ee51ed90179706768c990382a3ad
johnp808 <johnpuliatch@gmail.com>
2025-06-23 23:39:38 -1000

    finished logic for counting chars, working on print Answer. need to fix the logic with the ternary before moving on. currently printing a new line instead of a comma when dealing with multiple 'biggest letters'

--- 31-MostFrequentCharacter/src/com/john/CharacterChecker.java
+++ 31-MostFrequentCharacter/src/com/john/CharacterChecker.java
@@ -1,7 +1,20 @@
 package com.john;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
 public class CharacterChecker {
 
+	private Scanner sc = new Scanner(System.in);
+	private String sentence = "", userInput = "";
+	private char biggestLetter = ' ';
+	private int counter = 0, commaCount = 0;
+	private Map<Character, Integer> charsMap = new HashMap<>();
+	
+	
+	
 	public static void main(String[] args) {
 		CharacterChecker cC = new CharacterChecker();
 		cC.run();
@@ -16,15 +29,35 @@ public class CharacterChecker {
 	}
 	
 	public void userInput() {
-		
+		sentence = "Tt Aa Bb".toLowerCase();
 	}
 	
 	public void countChars() {
+		for (char letter : sentence.toCharArray()) {
+			if (Character.isLetter(letter)) {
+				charsMap.put(letter, charsMap.getOrDefault(letter, 0) + 1);
+			}
+		}
+		for (int i = 0; i < charsMap.size(); i++) {
+			for(int count : charsMap.values()) {
+				if (count > counter) {
+					counter = count;
+				}
+			}
 		
+		}
 	}
 	
 	public void printAnswer() {
-		
+		System.out.print("Most Frequent Letter(s): ");
+		for (Map.Entry<Character, Integer> entry : charsMap.entrySet()) {
+			if (entry.getValue() == counter) {
+				System.out.print(entry.getKey());
+				System.out.print(commaCount > counter ? ", " : "\n");
+				commaCount++;
+			}
+		}
+		System.out.println("Frequency: " + counter);
 	}
 	
 	public boolean runAgain() {
