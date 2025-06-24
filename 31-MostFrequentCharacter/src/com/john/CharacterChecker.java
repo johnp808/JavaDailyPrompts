ef31b222123873dc8bf3381b9a44f36da5358d27
johnp808 <johnpuliatch@gmail.com>
2025-06-23 21:31:33 -1000

    6-23-25 prompt created and boilerplate added for 'Most Frequent Character'

---
+++ 31-MostFrequentCharacter/src/com/john/CharacterChecker.java
@@ -0,0 +1,37 @@
+package com.john;
+
+public class CharacterChecker {
+
+	public static void main(String[] args) {
+		CharacterChecker cC = new CharacterChecker();
+		cC.run();
+	}
+	
+	public void run() {
+		do {
+			userInput();
+			countChars();
+			printAnswer();
+		} while(runAgain());
+	}
+	
+	public void userInput() {
+		
+	}
+	
+	public void countChars() {
+		
+	}
+	
+	public void printAnswer() {
+		
+	}
+	
+	public boolean runAgain() {
+		return false;
+	}
+	
+	public void reset() {
+		
+	}
+}
