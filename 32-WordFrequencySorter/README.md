0dfa953436287d78c37cf7105294c635544a0649
johnp808 <johnpuliatch@gmail.com>
2025-06-24 23:10:56 -1000

    Completed prompt for 6-24-25 and added README.md

---
+++ 32-WordFrequencySorter/README.md
@@ -0,0 +1,16 @@
+# Word Frequency Sorter
+
+### 6-24-25
+
+## Goal: Sort and display the words in a sentence by their frequency of occurrence in descending order.
+
+# Prompt Write a Java program that takes a sentence as input, counts the frequency of each word (case-insensitive), and prints the words sorted from most frequent to least frequent. If two words have the same frequency, maintain their original order of appearance.
+
+## Input Requirements:
+
+### A sentence containing words separated by spaces and/or punctuation (only letters should be considered part of words).
+### The input may contain duplicate words with varying cases.
+
+## Output Expectations:
+
+### A list of unique words and their frequencies sorted from highest to lowest frequency.
--- 32-WordFrequencySorter/src/com/john/WordFreqSorter.java
+++ 32-WordFrequencySorter/src/com/john/WordFreqSorter.java
@@ -1,16 +1,17 @@
 package com.john;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class WordFreqSorter {
 
 	private Scanner sc = new Scanner(System.in);
 	private String sentence = "", word = "", userInput = "";
-	private Map<String,Integer> wordCounts = new HashMap<String,Integer>();
-	private ArrayList<Map<String,Integer>> sortedWords = new ArrayList<Map<String,Integer>>();
+	private Map<String,Integer> wordCounts = new LinkedHashMap<String,Integer>();
+	private ArrayList<Map.Entry<String,Integer>> sortedWords;
 	
 	public static void main(String[] args) {
 		WordFreqSorter wFS = new WordFreqSorter();
@@ -18,35 +19,79 @@ public class WordFreqSorter {
 	}
 	
 	public void run() {
+		System.out.println("Please Enter A Sentence. I Will Then Print The Words From Greatest Frequency To Least: ");
 		do {
 			userInput();
-			addWordsToMap();
+			breakSentenceIntoWords();
 			sortWords();
 			printResults();
 		}while(runAgain());
 	}
 	
 	public void userInput() {
-		sentence = "The sun shines and the sun sets.";
+		while (true) {
+			sentence = sc.nextLine().toLowerCase();
+			if (sentence.isBlank()) {
+				System.out.println("Please Enter At Least One Word...");
+			}
+			else {
+				break;
+			}
+		}
 	}
 	
-	public void addWordsToMap() {
-		
+	public void breakSentenceIntoWords() {
+		for (int i = 0; i < sentence.length(); i++) {
+			char letter = sentence.charAt(i);
+			if (Character.isLetter(letter)) {
+				word += letter;
+			}
+			else if (letter == ' ') {
+				addWordToMap();
+			}
+		}
+		if (!word.isBlank()) {
+			addWordToMap();
+		}
+	}
+	
+	public void addWordToMap() {
+		wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+		word = "";
 	}
 	
 	public void sortWords() {
-		
-	}
-	
-	public boolean runAgain() {
-		return false;
+		sortedWords = new ArrayList<>(wordCounts.entrySet());
+		sortedWords.sort((word1, word2) -> word2.getValue().compareTo(word1.getValue()));
+
 	}
 	
 	public void printResults() {
-		
+		for (Entry<String, Integer> entry : sortedWords) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+	}
+	
+	public boolean runAgain() {
+		System.out.println("\nWould You Like To Run The Program Again?\n1) Yes\n2) No");
+		while (true) {
+			userInput = sc.nextLine();
+			if (userInput.equals("1")) {
+				System.out.println("Enter Antother Sentence: ");
+				reset();
+				return true;
+			}
+			else if (userInput.equals("2")) {
+				System.out.println("Goodbye");
+				return false;
+			}
+		}
 	}
 	
 	public void reset() {
-		
+		sentence = "";
+		word = "";
+		wordCounts.clear();
+		sortedWords.clear();
 	}
 }
