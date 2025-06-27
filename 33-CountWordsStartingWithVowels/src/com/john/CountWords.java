157b640012564e5bea3eb44aed50fed62d16d46d
johnp808 <johnpuliatch@gmail.com>
2025-06-26 20:25:17 -1000

    finished creating yesterdays prompt for 6-25-26 'Count Words Starting With Vowels' and boilerplate code

---
+++ 33-CountWordsStartingWithVowels/src/com/john/CountWords.java
@@ -0,0 +1,33 @@
+package com.john;
+
+public class CountWords {
+
+	public static void main(String[] args) {
+		CountWords cW = new CountWords();
+		cW.run();
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void userInput() {
+		
+	}
+	
+	public void countVowels() {
+		
+	}
+	
+	public void printResults() {
+		
+	}
+	
+	public boolean runAgain() {
+		return false;
+	}
+	
+	public void reset() {
+		
+	}
+}
