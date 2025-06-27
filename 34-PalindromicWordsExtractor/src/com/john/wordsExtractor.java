cc0bb9d3b81619a55fe684506cf7896c5396602e
johnp808 <johnpuliatch@gmail.com>
2025-06-26 21:47:59 -1000

    created boilerplate for prompt 6-26-26 ' Palindromic Words Extractor'

---
+++ 34-PalindromicWordsExtractor/src/com/john/wordsExtractor.java
@@ -0,0 +1,40 @@
+package com.john;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class wordsExtractor {
+
+	private Scanner sc = new Scanner(System.in);
+	private String sentence = "", word = "", userInput = "";
+	private Set<String> palindromes = new LinkedHashSet<>();
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void userInput () {
+		
+	}
+	
+	public void extractPalindromes() {
+		
+	}
+	
+	public void printAnswers() {
+		
+	}
+	
+	public boolean runAgain() {
+		return false;
+	}
+	
+	public void rest() {
+		
+	}
+}
