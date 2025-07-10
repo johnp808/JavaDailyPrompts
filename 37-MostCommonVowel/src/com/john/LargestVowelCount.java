c8862dd05da35c21b9672a871d774705d7e8d0f3
johnp808 <johnpuliatch@gmail.com>
2025-07-10 06:18:08 -1000

    Created prompt for 7-9-25 'Most Common Vowel' working on runAgain() and reset()

---
+++ 37-MostCommonVowel/src/com/john/LargestVowelCount.java
@@ -0,0 +1,80 @@
+package com.john;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LargestVowelCount {
+
+	private Scanner sc = new Scanner(System.in);
+	private String sentence = "", input = "";
+	private Map<Character,Integer> vowelsMap = new LinkedHashMap<>();
+	private int vowelCount = 0, largestVowels = 0;
+	
+	public static void main(String[] args) {
+		LargestVowelCount lVC = new LargestVowelCount();
+		lVC.run();
+	}
+	
+	public void run() {
+		System.out.println("Input Words And I Will Find The Largest Vowel(s) And Print The Results:");
+		do {
+			userInput();
+			countVowels();
+			findVowelFreqAndCount();
+			printAnswer();
+		}while(runAgain());
+	}
+	
+	public void userInput() {
+		sentence = sc.nextLine();
+	}
+	
+	public void countVowels() {
+		for (int i = 0; i < sentence.length(); i++) {
+			char letter = sentence.toLowerCase().charAt(i);
+			if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+				vowelsMap.put(letter, vowelsMap.getOrDefault(letter, 0) + 1);
+			}
+		}
+	}
+	
+	public void findVowelFreqAndCount() {
+		for (int count : vowelsMap.values()) {
+			if (count > vowelCount) {
+				vowelCount = count;
+			}
+		}
+		for (int count : vowelsMap.values()) {
+			if (count == vowelCount) {
+				largestVowels++;
+			}
+		}
+	}
+	
+	public void printAnswer() {
+		if (vowelsMap.isEmpty()) {
+			System.out.println("No Vowels Found.");
+		}
+		else {
+			System.out.print("Most Common Vowel" + (largestVowels > 1 ? "s: " : ": "));
+			char vowel = ' ';
+			int commaCount = 0;
+			for (Map.Entry<Character, Integer> entry : vowelsMap.entrySet()) {
+				if (entry.getValue() == vowelCount) {
+					vowel = entry.getKey();
+					commaCount++;
+					System.out.print(vowel + (commaCount < largestVowels ? ", " : "\n"));
+				}
+			}
+		}
+	}
+	
+	public boolean runAgain() {
+		return false;
+	}
+	
+	public void reset() {
+		
+	}
+}
