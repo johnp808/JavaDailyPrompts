b782489ee8ac0a90272589411dda424604db2e5a
johnp808 <johnpuliatch@gmail.com>
2025-07-10 20:53:55 -1000

    created prompt for 7-10-25 'Unique Special Character Extractor' with boilerplate

---
+++ 38-UniqueSpecialCharactersExtractor/src/com/john/SpecialCharExtractor.java
@@ -0,0 +1,34 @@
+package com.john;
+
+public class SpecialCharExtractor {
+
+	public static void main(String[] args) {
+		SpecialCharExtractor sCE = new SpecialCharExtractor();
+		sCE.run();
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void userInput() {
+		
+	}
+	
+	public void checkSpecialChars() {
+		
+	}
+	
+	public void printAnswer() {
+		
+	}
+	
+	public boolean runAgain() {
+		return false;
+	}
+	
+	public void rest() {
+		
+	}
+	
+}
