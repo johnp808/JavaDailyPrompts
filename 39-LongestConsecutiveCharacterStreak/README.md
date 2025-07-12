9f2fd6ec6c2e1d47e6b5db71427bc9d3879950dd
johnp808 <johnpuliatch@gmail.com>
2025-07-12 11:04:34 -1000

    actually completed the prompt, included user input and runAgain() logic. Completed README.md

---
+++ 39-LongestConsecutiveCharacterStreak/README.md
@@ -0,0 +1,15 @@
+# Longest Consecutive Character Streak
+
+### 7-12-25
+
+## Goal: Write a program that finds the longest streak of a single repeated character in a sentence.
+
+# Prompt: Given a string, find the character that appears consecutively the most times in a row, along with the length of that streak.
+
+## Accept a sentence from the user.
+
+## Case-insensitive
+
+## Ignore spaces and punctuation
+
+## Report the first longest streak if there’s a tie
--- 39-LongestConsecutiveCharacterStreak/src/com/john/LongestConsecutiveCharStreak.java
+++ 39-LongestConsecutiveCharacterStreak/src/com/john/LongestConsecutiveCharStreak.java
@@ -1,14 +1,12 @@
 package com.john;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class LongestConsecutiveCharStreak {
 
 	private Scanner sc = new Scanner(System.in);
 	private String sentence = "", userInput = "";
-	private char nextLetter = ' ', previousLetter =' ', streakChar = ' ';
+	private char previousLetter =' ', streakChar = ' ';
 	private int streakCount = 0, maxCount = 1;
 	public static void main(String[] args) {
 		LongestConsecutiveCharStreak lCCS = new LongestConsecutiveCharStreak();
@@ -16,18 +14,19 @@ public class LongestConsecutiveCharStreak {
 	}
 	
 	public void run() {
+		System.out.println("Please Enter A Word Or Sentence And I Will Find The Longest Consecutive Letter: ");
 		do {
 			userInput();
-			check();
+			countChars();
 			printAnswer();
 		}while(runAgain());
 	}
 	
 	public void userInput() {
-		sentence = "AAaaBBBBccCDDDD";
+		sentence = sc.nextLine();
 	}
 	
-	public void check() {
+	public void countChars() {
 		for (int i = 0; i < sentence.length(); i++) {
 			char currentLetter = sentence.toLowerCase().charAt(i);
 			if( i != 0) {
@@ -56,11 +55,26 @@ public class LongestConsecutiveCharStreak {
 	}
 	
 	public boolean runAgain() {
-		return false;
+		while (true) {
+			System.out.println("\nWould You Like To Run It Again?\n1) Yes\n2) No");
+			userInput = sc.nextLine();
+			if (userInput.equals("1")) {
+				System.out.println("Enter A New Sentence Or Word: ");
+				reset();
+				return true;
+			}
+			else if (userInput.equals("2")) {
+				return false;
+			}
+			else {
+				System.out.println("Please Enter A Valid Option...");
+			}
+		}
 	}
 	
 	public void reset() {
-		
+		streakChar = ' ';
+		streakCount = 0;
+		maxCount = 0;
 	}
-	
 }
