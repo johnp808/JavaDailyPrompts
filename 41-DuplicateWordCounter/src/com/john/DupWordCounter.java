cbfd3c750bb8444cac822688905d73be1d36acd0
johnp808 <johnpuliatch@gmail.com>
2025-07-13 23:24:40 -1000

    completed logic for methods labeled findDups, countDups and printAnswers

--- 41-DuplicateWordCounter/src/com/john/DupWordCounter.java
+++ 41-DuplicateWordCounter/src/com/john/DupWordCounter.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 public class DupWordCounter {
 
 	private Scanner sc = new Scanner(System.in);
-	private String sentence = "", userInput = "", word = "";
+	private String sentence = "", userInput = "", word = "", duplicateWord = "";
 	private Map<String, Integer> wordsMap = new LinkedHashMap<>();
+	private int counter = 0, dupCount = 0;
 	
 	public static void main(String[] args) {
 		DupWordCounter dWC = new DupWordCounter();
@@ -19,20 +20,50 @@ public class DupWordCounter {
 		do {
 			userInput();
 			findDups();
+			countDups();
 			printAnswers();
 		}while(runAgain());
 	}
 	
 	public void userInput() {
-		
+		sentence = "The cat chased the mouse but the cat ran fast.";
 	}
 	
 	public void findDups() {
+		for (int i = 0; i < sentence.length(); i++) {
+			char letter = sentence.toLowerCase().charAt(i);
+			if(Character.isLetter(letter)) {
+				word += letter;
+			}
+			else if (letter == ' ') {
+				wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+				word = "";
+			}
+		}
 		
+		if (!word.isBlank()) {
+			wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+			word = "";
+		}
+	}
+	
+	public void countDups() {
+		for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+			if (entry.getValue() > 1) {
+				dupCount++;
+			}
+		}
 	}
 	
 	public void printAnswers() {
-		
+		System.out.println("Total Repeated Words: " + dupCount);
+		for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+			if (entry.getValue() > 1) {
+				duplicateWord = entry.getKey();
+				dupCount = entry.getValue();
+				System.out.println(duplicateWord + ": " + dupCount);
+			}
+		}
 	}
 	
 	public boolean runAgain() {
