9c91ae3483217af648b1a5e4262c19ab44e92440
johnp808 <johnpuliatch@gmail.com>
2025-07-21 20:47:59 -1000

    completed method count()

--- 44-WordLengthAverageCalculator/src/com/john/WordLengthAvgCalc.java
+++ 44-WordLengthAverageCalculator/src/com/john/WordLengthAvgCalc.java
@@ -6,7 +6,7 @@ public class WordLengthAvgCalc {
 
 	private Scanner sc = new Scanner(System.in);
 	private String userInput = "", sentence = "", word = "";
-	private int totalLetters = 0, totalWords = 0;
+	private double totalLetters = 0, totalWords = 0, average = 0;
 	
 	
 	public static void main(String[] args) {
@@ -24,15 +24,28 @@ public class WordLengthAvgCalc {
 	}
 	
 	public void userInput() {
-		sentence = "";
+		sentence = "The quick brown fox jumps over the lazy dog.".toLowerCase();
 	}
 	
 	public void count() {
-		
+		for (int i = 0; i < sentence.length(); i++) {
+			char letter = sentence.charAt(i);
+			if (Character.isLetter(letter)) {
+				word += letter;
+			}
+			else if (letter == ' ') {
+				totalLetters += word.length();
+				totalWords++;
+				word = "";
+			}
+		}
+		if (!word.isBlank()) {
+			totalLetters += word.length();
+			totalWords++;
+		}
 	}
 	
 	public void calculate() {
-		
 	}
 	
 	public void printAnswer() {
